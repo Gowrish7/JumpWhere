ce783c2a99d5489834d8d53058f5a449855b4c6a
Gowrish_77 <103709921+Gowrish7@users.noreply.github.com>
2023-12-19 22:15:10 +0530

    Update 9.3.py

--- Assigment 9.3/9.3.py
+++ Assigment 9.3/9.3.py
@@ -8,7 +8,6 @@ def is_balanced_delimiters(input_string):
         elif char in ')]}':
             if not stack or stack.pop() != mapping[char]:
                 return False
-
     return len(stack) == 0
 input_str1 = "([{}])"
 input_str2 = "([)]"
